1b98afb81da94639cba4ad77819f8119b7386b01
Guo Shiwei <guoshiwei@gmail.com>
2015-05-09 13:15:08 +0800

    Run Docker using DockerRunnerApplicationMaster succeed.

--- distributed-docker-submit-client/src/main/java/com/sogou/docker/client/docker/LocalDockerContainerRunner.java
+++ distributed-docker-submit-client/src/main/java/com/sogou/docker/client/docker/LocalDockerContainerRunner.java
@@ -1,5 +1,6 @@
 package com.sogou.docker.client.docker;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,41 +12,49 @@ import java.util.Map;
  * TODO: Implement LocalDockerContainerRunner
  */
 public class LocalDockerContainerRunner {
+  private final YarnDockerClient client;
   private float progress;
   private volatile boolean done = false;
 
-  public static class DockerOptions {
-    public String imageName ;
-    public int containerCpuShares;
-    public int containerMemoryLimit; // In bytes
-    public String command;
-
-    public String containerDataDir; // If given, `localDataDir` will be mapped into
-                                        // `containerDataDir` of docker container.
-    public String localDataDir;
-    public String workingDir;
-
-    public String runnerScriptPath; // The absolute path of runner script on host local filesystem
-
-    // Env variables to be setup for the shell command
-    public Map<String, String> cmdEnv = new HashMap<String, String>();
-  }
+  private YarnDockerClientParam yarnDockerClientParam;
+  private int exitStatus = -1;
 
   public boolean isFinshed(){
     return done;
   }
 
   public int getExitStatus(){
-    return 0;
+    return exitStatus;
   }
 
   public void ensureContainerRemoved() {
 
   }
 
-  public void run() {
+  public LocalDockerContainerRunner() {
 
-    System.err.println("Hello from docker on yarn");
+    yarnDockerClientParam = buildYarnDockerClientParam();
+    client = new YarnDockerClient(yarnDockerClientParam);
+  }
+
+  private YarnDockerClientParam buildYarnDockerClientParam() {
+    YarnDockerClientParam p = new YarnDockerClientParam();
+
+    // TODO Get those param from yarn
+    p.cmdAndArgs = "echo Hello from docker on yarn".split("\\s+");
+    p.containerMemory = 512;
+    p.containerVirtualCores = 1;
+    p.runnerScriptPath = "/Users/guoshiwei/DEV/docker-client/runner.py";
+    p.dockerCertPath = "/Users/guoshiwei/.boot2docker/certs/boot2docker-vm";
+    p.dockerHost = "192.168.59.103:2376";
+    p.dockerImage = "registry.docker.dev.sogou-inc.com:5000/clouddev/sogou-rhel-base:6.5";
+    return p;
+  }
+
+  public void run() throws IOException {
+
+    exitStatus = client.runtask();
+    done = true;
     progress = 100;
   }
 
--- distributed-docker-submit-client/test_docker_client.sh
+++ distributed-docker-submit-client/test_docker_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./hadoop-env.sh
 
-AM_JAR=target/distributed-docker-submit-client-0.0.1-SNAPSHOT.jar
+AM_JAR=target/distributed-docker-submit-client-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 CLIENT_JAR=$AM_JAR
 ~/DEV/ENV/hadoop-2.7.0/bin/hadoop jar $CLIENT_JAR com.sogou.docker.client.DockerClientV2 \
     -jar $AM_JAR $@
